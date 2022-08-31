3de36a5e2b135ad1c007dfc2829e670460e768ed
yoonseok0323 <yoonrock02@gmail.com>
2022-08-31 16:03:29 +0900

    update

--- lesson2/main.py
+++ lesson2/main.py
@@ -12,6 +12,7 @@ def hello_world():
 
 if __name__ == "__main__":
     import uvicorn
+    
     #uvicorn.run(app, host="0.0.0.0", port=8000)
     uvicorn.run(app, host="0.0.0.0", port=int(os.environ["PORT"]))
     # Env 설정을 통해 port 번호를 입력하지 않고 os에서 불러와서 동작하도록 한다.
